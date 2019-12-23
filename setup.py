94b5eba9f9f33ed25eb3f5dd5b062670d220c55c
vladyslavZavadskyy <hort.hope@gmail.com>
2019-12-23 15:43:34 +0200

    Fix, hopefully

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import os
 
 INSTALL_REQUIREMENTS = ['torch', 'pandas', 'tornado']
 
@@ -11,4 +12,11 @@ setup(
 
     packages=find_packages(),
     install_requires=INSTALL_REQUIREMENTS,
+    package_data={
+        'training_control': [
+            os.path.join('static', 'main.js'),
+            os.path.join('static', 'main.css'),
+            os.path.join('templates', 'index.html'),
+        ]
+    },
 )
